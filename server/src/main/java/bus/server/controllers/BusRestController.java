0516b1f46f4f89bff1e6b7709a43de6da61721cc
unknown <songyanghan+git@gmail.com>
2022-02-16 12:58:07 +0800

    EDIT: minor qol changes

--- server/src/main/java/bus/server/controllers/BusRestController.java
+++ server/src/main/java/bus/server/controllers/BusRestController.java
@@ -17,9 +17,6 @@ import jakarta.json.JsonObject;
 @RequestMapping(path="/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BusRestController {
 
-    String demo = """
-    {"Message": "DEMOOOOO", "odata.metadata":"http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element","BusStopCode":"20239","Services":[{"ServiceNo":"143M","Operator":"TTS","NextBus":{"OriginCode":"28009","DestinationCode":"28009","EstimatedArrival":"2022-02-16T04:55:06+08:00","Latitude":"1.3186863333333334","Longitude":"103.74820733333334","VisitNumber":"1","Load":"SDA","Feature":"WAB","Type":"SD"},"NextBus2":{"OriginCode":"28009","DestinationCode":"28009","EstimatedArrival":"2022-02-16T05:18:17+08:00","Latitude":"0","Longitude":"0","VisitNumber":"1","Load":"LSD","Feature":"WAB","Type":"SD"},"NextBus3":{"OriginCode":"","DestinationCode":"","EstimatedArrival":"","Latitude":"","Longitude":"","VisitNumber":"","Load":"","Feature":"","Type":""}}]} """;
-
     @Autowired
     BusService busService;
 
@@ -28,10 +25,8 @@ public class BusRestController {
     
     @GetMapping(path="/BusStop/{busstopId}")
     public ResponseEntity<String> getBusStopArrival(@PathVariable String busstopId) {
-        //String jsonData = busService.getBusStopById(busstopId);
-        //return ResponseEntity.ok(jsonData);
-
-        return ResponseEntity.ok(this.demo);
+        String jsonData = busService.getBusStopById(busstopId);
+        return ResponseEntity.ok(jsonData);
     }
 
     @GetMapping(path="/BusStops")
